bfe22dfdf764ff7f2f55427f8b2ef70685b6742f
Alona Trekhlib <trekhleb123@Alonas-MacBook-Air.local>
2020-04-14 10:18:12 -0500

    linked to prisma db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga")
+const { prisma } = require("./generated/prisma-client")
 const _find = require("lodash/find")
+
 //dummy data for links
 // let links = [
 //   {
@@ -9,16 +11,19 @@ const _find = require("lodash/find")
 //   },
 // ]
 
-let idCount = links.length
+//let idCount = links.length
 
 //actual implementation of the GraphQL schema
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, arg) => {
-      return _find(links, { id: arg.id })
+    feed: (root, arg, context, info) => {
+      console.log(context)
+      context.prisma.links()
     }
+    // link: (parent, arg) => {
+    //   return _find(links, { id: arg.id })
+    // }
   },
   // Link: {
   //   //parent (or sometimes root or obj) is the result of the previous resolver execution level.
@@ -27,31 +32,29 @@ const resolvers = {
   //   url: (parent) => parent.url,
   // },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
-    },
-    updateLink: (parent, arg) => {
-      const link = _find(links, { id: arg.id })
-      link.url = arg.url
-      link.description = arg.description
-      return link
-    },
-    deleteLink: (parent, arg) => {
-      const deletedLink = _find(links, { id: arg.id })
-      links = links.filter(link => link.id !== arg.id)
-      return deletedLink
+    post: (parent, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      })
     }
+    // updateLink: (parent, arg) => {
+    //   const link = _find(links, { id: arg.id })
+    //   link.url = arg.url
+    //   link.description = arg.description
+    //   return link
+    // },
+    // deleteLink: (parent, arg) => {
+    //   const deletedLink = _find(links, { id: arg.id })
+    //   links = links.filter(link => link.id !== arg.id)
+    //   return deletedLink
+    // }
   }
 }
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
